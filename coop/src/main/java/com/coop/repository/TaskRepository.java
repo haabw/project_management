1f07d738658fb575c61a53e5f61a07eca83618ee
haabw <yhb349@naver.com>
2025-05-02 11:43:58 +0900

    Delete coop/src/main/java/com/coop/repository/TaskRepository.java

--- coop/src/main/java/com/coop/repository/TaskRepository.java
+++
@@ -1,8 +0,0 @@
-package com.coop.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.coop.entity.TaskEntity;
-
-public interface TaskRepository extends JpaRepository<TaskEntity, Integer> {}
